a45a9966a703824492a10d032642ea0a5b1bc6cd
SunDoge <384813529@qq.com>
2019-09-21 21:13:33 +0800

    timeout and more connections

--- download_train.sh
+++ download_train.sh
@@ -1,1 +1,1 @@
-proxychains4 aria2c -i train.txt -d train --async-dns=false --save-session train-session.txt
+proxychains4 aria2c -i train.txt -d train --async-dns=false --save-session train-session.txt -j 32 -t 10
--- download_val.sh
+++ download_val.sh
@@ -1,1 +1,1 @@
-proxychains4 aria2c -i val.txt -d val --async-dns=false --save-session val-session.txt
+proxychains4 aria2c -i val.txt -d val --async-dns=false --save-session val-session.txt -j 32 -t 10
